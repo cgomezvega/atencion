d257427fa5e95e6b0d8fb0fce32303f41d7a53dd
unknown <CETECOM@AOAWSB03LC0900.duoc.acad>
2025-05-23 14:01:44 -0400

    Creado comentario en crated

--- src/main/java/com/hospital/atencion/dto/CourseCategory.java
+++ src/main/java/com/hospital/atencion/dto/CourseCategory.java
@@ -7,6 +7,6 @@ public class CourseCategory {
     public Integer id;
     public String name;
     public String description;
-    public Instant created;
+    public Instant created; //Propiedad para saber cuando se creó la categoría de curso
 
 }

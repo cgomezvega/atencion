625d2768fa7110d2573a0b95b16aafcb64b8c583
unknown <CETECOM@AOAWSB03LC0900.duoc.acad>
2025-05-23 13:52:06 -0400

    Actualizado DTO CourseCategory con nueva propiedad created

--- src/main/java/com/hospital/atencion/dto/CourseCategory.java
+++ src/main/java/com/hospital/atencion/dto/CourseCategory.java
@@ -1,9 +1,12 @@
 package com.hospital.atencion.dto;
 
+import java.time.Instant;
+
 public class CourseCategory {
     
     public Integer id;
     public String name;
     public String description;
+    public Instant created;
 
 }
